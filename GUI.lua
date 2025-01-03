ef0bb23dc00f33051b40aebffa001b65abdd7c9f
Mick Gordon <78802703+Mick-gordon@users.noreply.github.com>
2025-01-03 22:44:08 +0000

    Update GUI.lua

--- GUI.lua
+++ GUI.lua
@@ -291,12 +291,12 @@ function Libary:CreateWindow(Name, Toggle, keybind)
 
 		ColorPicker.Saturation.MouseButton1Down:Connect(function()
 			ColorPicker.SlidingSaturation = true;
-			ColorPicker.SlideSaturation({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 38)});
+			ColorPicker.SlideSaturation({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 42)});
 		end);
 
 		ColorPicker.Saturation.InputChanged:Connect(function()
 			if ColorPicker.SlidingSaturation then
-				ColorPicker.SlideSaturation({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 38)});
+				ColorPicker.SlideSaturation({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 42)});
 			end;
 		end);
 
@@ -319,12 +319,12 @@ function Libary:CreateWindow(Name, Toggle, keybind)
 
 		ColorPicker.Hue.MouseButton1Down:Connect(function()
 			ColorPicker.SlidingHue = true;
-			ColorPicker.SlideHue({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 38)});
+			ColorPicker.SlideHue({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 42)});
 		end);
 
 		ColorPicker.Hue.InputChanged:Connect(function()
 			if ColorPicker.SlidingHue then
-				ColorPicker.SlideHue({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 38)});
+				ColorPicker.SlideHue({ Position = UserInputService:GetMouseLocation() - Vector2.new(0, 42)});
 			end;
 		end);
 
